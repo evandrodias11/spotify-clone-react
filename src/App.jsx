d0550d1e85edaf6cb6f57273a9d0eda9c537f96c
evandrodias11 <evandro.dias@paripassu.com.br>
2024-02-13 16:17:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Footer from "./Components/Footer/Footer";
+import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import Sidebar from "./Components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
